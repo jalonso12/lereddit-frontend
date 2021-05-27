89add97d94ef507f9d9635e2be464793014dc2a0
jalonso12 <jalonsoo514@ulacit.ed.cr>
2021-05-26 21:48:34 -0600

    Login Form
    - Created component for input fields
    
    - Applied component to register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,8 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import React from 'react';
+import InputField from '../components/InputField';
 import Wrapper from '../components/Wrapper';
 
 interface registerProps {}
@@ -14,18 +16,29 @@ export const register: React.FC<registerProps> = ({}) => {
           console.log(values);
         }}
       >
-        {({ values, handleChange }) => (
+        {({ isSubmitting }) => (
           <Form>
-            <FormControl>
-              <FormLabel htmlFor="username">Username</FormLabel>
-              <Input
-                value={values.username}
-                onChange={handleChange}
-                id="username"
-                placeholder="username"
-              />
-              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-            </FormControl>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            ></InputField>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              register
+            </Button>
           </Form>
         )}
       </Formik>
